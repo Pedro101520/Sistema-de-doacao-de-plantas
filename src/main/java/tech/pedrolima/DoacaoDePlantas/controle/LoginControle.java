48fb309a6bf2a686dffbc5bc98021f36ce71da26
Pedro101520 <pedrolimazoi10@gamil.com>
2024-08-16 11:08:11 -0300

    Back-end da tele de login

--- src/main/java/tech/pedrolima/DoacaoDePlantas/controle/LoginControle.java
+++ src/main/java/tech/pedrolima/DoacaoDePlantas/controle/LoginControle.java
@@ -1,15 +1,36 @@
 package tech.pedrolima.DoacaoDePlantas.controle;
 
+import org.springframework.ui.Model;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
+import tech.pedrolima.DoacaoDePlantas.modelos.Cadastro;
+import tech.pedrolima.DoacaoDePlantas.repositorios.CadastroRepositorio;
 
 @Controller
 public class LoginControle {
+
+    @Autowired
+    private CadastroRepositorio cadastroRepositorio;
+
     @GetMapping("/login")
     public ModelAndView cadastrar(){
         ModelAndView mv = new ModelAndView("logins/login");
-//        mv.addObject("usuario", usuario);
         return mv;
+    }
+
+    @PostMapping("/logar")
+    //Aqui estou criando uma variavel do tipo Cadastro (classe modelo) para armazenar as informações de login
+    public String logar(Model model, Cadastro cadParam){
+        //Estou acessando os dados do banco de dados
+        Cadastro cad = this.cadastroRepositorio.login(cadParam.getEmail(), cadParam.getSenha());
+        if(cad != null){
+            return "pages/MainScreen";
+        }
+        model.addAttribute("erro", "Usuário ou senha inválidos");
+        return "logins/login";
     }
 }
--- src/main/java/tech/pedrolima/DoacaoDePlantas/repositorios/CadastroRepositorio.java
+++ src/main/java/tech/pedrolima/DoacaoDePlantas/repositorios/CadastroRepositorio.java
@@ -1,8 +1,13 @@
 package tech.pedrolima.DoacaoDePlantas.repositorios;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import tech.pedrolima.DoacaoDePlantas.modelos.Cadastro;
 
 public interface CadastroRepositorio extends JpaRepository<Cadastro, Long> {
+
+    @Query(value = "SELECT * FROM usuario WHERE email = :email AND senha = :senha", nativeQuery = true)
+    public Cadastro login(String email, String senha);
 
 }
